041f3cb511913f5a01fd815db2d7521fbec16549
ariangh001 <ariangh001@gmail.com>
2019-05-13 16:03:13 +0430

    implemented commandExists and questionExists functions

--- InputHandler.cpp
+++ InputHandler.cpp
@@ -30,7 +30,7 @@ void InputHandler::getInput()
 
 void InputHandler::checkSyntaxErrors(std::vector<std::string> words)
 {
-    checkFirstParameter(words[0]);
+    checkFirstParameter(words[COMMAND_INDEX]);
     commandExists(words);
     checkParameters(words);
 }
@@ -42,7 +42,73 @@ void InputHandler::checkParameters(std::vector<std::string> words)
 
 void InputHandler::commandExists(std::vector<std::string> words)
 {
-
+    if(questionMarkExists(words) == true)
+    {
+        if(words[2] == "?")
+        {
+            if(words[COMMAND_INDEX] == POST_COMMAND)
+            {
+                if(words[1] != "signup"
+                && words[1] != "login"
+                && words[1] != "films"
+                && words[1] != "replies"
+                && words[1] != "followers"
+                && words[1] != "money"
+                && words[1] != "buy"
+                && words[1] != "rate"
+                && words[1] != "comments")
+                    throw BadRequest();
+            }
+            else if(words[COMMAND_INDEX] == PUT_COMMAND)
+            {
+                if(words[1] != "films"
+                && words[1] != "signup")
+                    throw BadRequest();
+            }
+            else if(words[COMMAND_INDEX] == GET_COMMAND)
+            {
+                if(words[1] != "published"
+                && words[1] != "films"
+                && words[1] != "purchased")
+                    throw BadRequest();
+            }
+            else if(words[COMMAND_INDEX] == DELETE_COMMAND)
+            {
+                if(words[1] != "comments"
+                && words[1] != "films")
+                    throw BadRequest();
+            }
+        }
+        else if(words[3] == "?")
+        {
+            if(!(words[1] == "notifications" && words[2] == "read"))
+                throw BadRequest();
+        }
+        else
+            throw BadRequest();
+    }
+    else
+    {
+        if(words[COMMAND_INDEX] == POST_COMMAND)
+        {
+            if(words[1] != "money")
+                throw BadRequest();
+        }
+        else if(words[COMMAND_INDEX] == PUT_COMMAND)
+        {
+            throw BadRequest();
+        }
+        else if(words[COMMAND_INDEX] == GET_COMMAND)
+        {
+            if(words[1] != "followers"
+            && words[1] != "notitfications")
+                throw BadRequest();
+        }
+        else if(words[COMMAND_INDEX] == DELETE_COMMAND)
+        {
+            throw BadRequest();
+        }
+    }
 }
 
 void InputHandler::checkFirstParameter(std::string command)
@@ -57,4 +123,12 @@ void InputHandler::checkFirstParameter(std::string command)
 std::vector<std::string> InputHandler::seperateString(std::string line)
 {
 
+}
+
+bool InputHandler::questionMarkExists(std::vector<std::string> words)
+{
+    for(Counter i=0; i<words.size(); i++)
+        if(words[i] == "?")
+            return true;
+    return false;
 }

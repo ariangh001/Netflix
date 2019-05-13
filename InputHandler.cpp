c5c9a6627042bd9cbbccfde7662423c55a8309d9
ariangh001 <ariangh001@gmail.com>
2019-05-13 16:59:23 +0430

    added string seperator function

--- InputHandler.cpp
+++ InputHandler.cpp
@@ -1,4 +1,5 @@
 #include "InputHandler.h"
+using namespace std;
 
 InputHandler::InputHandler()
 {
@@ -13,9 +14,9 @@ void InputHandler::start()
         {
             getInput();
         }
-        catch(std::exception e)
+        catch(exception e)
         {
-            std::cout << e.what() << '\n';
+            cout << e.what() << '\n';
         }
     }
 }
@@ -23,20 +24,20 @@ void InputHandler::start()
 void InputHandler::getInput()
 {
     input line;
-    getline(std::cin,line);
-    std::vector<std::string> words = seperateString(line);
+    getline(cin,line);
+    CommandList words = seperateString(line);
     checkSyntaxErrors(words);
-    
+
 }
 
-void InputHandler::checkSyntaxErrors(std::vector<std::string> words)
+void InputHandler::checkSyntaxErrors(CommandList words)
 {
-    checkFirstParameter(words[COMMAND_INDEX]);
+    checkFirstParameter(words[COMMAND_TYPE_INDEX]);
     commandExists(words);
     checkParameters(words);
 }
 
-void InputHandler::checkParameters(std::vector<std::string> words)
+void InputHandler::checkParameters(CommandList words)
 {
     if(questionMarkExists(words) == true)
     {
@@ -54,13 +55,13 @@ void InputHandler::checkParameters(std::vector<std::string> words)
     }
 }
 
-void InputHandler::commandExists(std::vector<std::string> words)
+void InputHandler::commandExists(CommandList words)
 {
     if(questionMarkExists(words) == true)
     {
         if(words[2] == "?")
         {
-            if(words[COMMAND_INDEX] == POST_COMMAND)
+            if(words[COMMAND_TYPE_INDEX] == POST_COMMAND)
             {
                 if(words[1] != "signup"
                 && words[1] != "login"
@@ -73,20 +74,20 @@ void InputHandler::commandExists(std::vector<std::string> words)
                 && words[1] != "comments")
                     throw NotFound();
             }
-            else if(words[COMMAND_INDEX] == PUT_COMMAND)
+            else if(words[COMMAND_TYPE_INDEX] == PUT_COMMAND)
             {
                 if(words[1] != "films"
                 && words[1] != "signup")
                     throw NotFound();
             }
-            else if(words[COMMAND_INDEX] == GET_COMMAND)
+            else if(words[COMMAND_TYPE_INDEX] == GET_COMMAND)
             {
                 if(words[1] != "published"
                 && words[1] != "films"
                 && words[1] != "purchased")
                     throw NotFound();
             }
-            else if(words[COMMAND_INDEX] == DELETE_COMMAND)
+            else if(words[COMMAND_TYPE_INDEX] == DELETE_COMMAND)
             {
                 if(words[1] != "comments"
                 && words[1] != "films")
@@ -103,43 +104,45 @@ void InputHandler::commandExists(std::vector<std::string> words)
     }
     else
     {
-        if(words[COMMAND_INDEX] == POST_COMMAND)
+        if(words[COMMAND_TYPE_INDEX] == POST_COMMAND)
         {
             if(words[1] != "money")
                 throw NotFound();
         }
-        else if(words[COMMAND_INDEX] == PUT_COMMAND)
+        else if(words[COMMAND_TYPE_INDEX] == PUT_COMMAND)
         {
             throw NotFound();
         }
-        else if(words[COMMAND_INDEX] == GET_COMMAND)
+        else if(words[COMMAND_TYPE_INDEX] == GET_COMMAND)
         {
             if(words[1] != "followers"
             && words[1] != "notitfications")
                 throw NotFound();
         }
-        else if(words[COMMAND_INDEX] == DELETE_COMMAND)
+        else if(words[COMMAND_TYPE_INDEX] == DELETE_COMMAND)
         {
             throw NotFound();
         }
     }
 }
 
-void InputHandler::checkFirstParameter(std::string command)
+void InputHandler::checkFirstParameter(string first_command)
 {
-    if(command != "POST"
-    && command != "GET"
-    && command != "DELETE"
-    && command != "PUT")
+    if(first_command != "POST"
+    && first_command != "GET"
+    && first_command != "DELETE"
+    && first_command != "PUT")
         throw BadRequest();
 }
 
-std::vector<std::string> InputHandler::seperateString(std::string line)
+CommandList InputHandler::seperateString(string line)
 {
-
+    stringstream input_stream(line);
+    return vector<string>(istream_iterator<string>(input_stream),
+                                        istream_iterator<string>());
 }
 
-bool InputHandler::questionMarkExists(std::vector<std::string> words)
+bool InputHandler::questionMarkExists(CommandList words)
 {
     for(Counter i=0; i<words.size(); i++)
         if(words[i] == "?")

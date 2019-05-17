45c689f9ecfad5d38366d7a401d11240bfbbdc90
ariangh001 <ariangh001@gmail.com>
2019-05-17 11:18:56 +0430

    Added a define for OK message

--- ExceptionHandler.h
+++ ExceptionHandler.h
@@ -4,23 +4,23 @@
 #define NOT_FOUND "Not Found"
 #define BAD_REQUEST "Bad Request"
 #define PERMISSION_DENIED "Permission Denied"
-
+#define OK_REQUEST "OK"
 class NotFound : public std::exception
 {
     public:
-        virtual const char* what() const throw(); 
+        virtual const char* what() const throw();
 };
 
 class BadRequest : public std::exception
 {
     public:
-        virtual const char* what() const throw(); 
+        virtual const char* what() const throw();
 };
 
 class PermissionDenied : public std::exception
 {
     public:
-        virtual const char* what() const throw(); 
+        virtual const char* what() const throw();
 };
 
 #endif

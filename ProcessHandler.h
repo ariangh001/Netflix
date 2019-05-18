802aa80496461698dd34bc92b8fddab123ceae6b
ariangh001 <ariangh001@gmail.com>
2019-05-18 10:55:56 +0430

    added some methods and repositories

--- ProcessHandler.h
+++ ProcessHandler.h
@@ -1,7 +1,16 @@
 #ifndef PROCESS_HANDLER
 #define PROCESS_HANDLER
 #include "User.h"
+#include "Publisher.h"
+#include "ExceptionHandler.h"
+#include "UsersRepository.h"
+#include "MovieRepository.h"
+#include "CommentRepository.h"
 #include <map>
+#include <iterator>
+#define HAS_ERROR 1
+#define NO_ERROR 0
+typedef unsigned int Counter;
 typedef std::map<std::string,std::string> Map;
 class ProcessHandler
 {
@@ -9,8 +18,13 @@ class ProcessHandler
         ProcessHandler();
         void signup(Map signupInput);
         void login(Map loginInput);
-        void checkFunctions(std::string,Map input);
+        void checkFunctions(std::string function_type,Map input);
     private:
         User* active_user;
+        //void checkPermission(std::string function_type);
+        void checkValues(Map input);
+        void checkEmailSyntax(std::string email);
+        bool isNumber(std::string str);
+        bool isFloat(std::string str);
 };
 #endif

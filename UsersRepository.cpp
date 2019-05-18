4132ea9125f95fb20a3e049347396711159afe62
ariangh001 <ariangh001@gmail.com>
2019-05-18 20:20:24 +0430

    added a constructor and edited addUser method

--- UsersRepository.cpp
+++ UsersRepository.cpp
@@ -1,7 +1,14 @@
 #include "UsersRepository.h"
 
+UsersRepository::UsersRepository()
+{
+    last_id = 0;
+}
+
 void UsersRepository::addUser(User* user)
 {
+    last_id++;
+    user->setId(last_id);
     users.push_back(user);
 }
 

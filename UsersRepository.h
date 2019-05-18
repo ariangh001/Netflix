0cc3d4b3c2213771b9e0e97b262eae2892e7782d
ariangh001 <ariangh001@gmail.com>
2019-05-18 19:53:33 +0430

    added some methods

--- UsersRepository.h
+++ UsersRepository.h
@@ -1,12 +1,16 @@
 #ifndef USERS_REPOSITORY_H
 #define USERS_REPOSITORY_H
 #include "User.h"
+#include "ExceptionHandler.h"
 #include <vector>
+#include <iostream>
+typedef unsigned int Counter;
 class UsersRepository
 {
     public:
         void addUser(User* new_user);
         User* findUser(std::string username, std::string password);
+        int getUsersNumber();
     private:
         std::vector<User*> users;
 };

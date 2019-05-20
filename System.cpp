2c92a1aa7fea4a5d909f33a4b2344ca41e89644a
ariangh001 <ariangh001@gmail.com>
2019-05-20 17:39:49 +0430

    made repositories dynamic

--- System.cpp
+++ System.cpp
@@ -2,6 +2,9 @@
 
 System::System()
 {
+    users_repo = new UsersRepository();
+    movie_repo = new MovieRepository();
+    comment_repo = new CommentRepository();
     process_handler = new ProcessHandler(users_repo,movie_repo,comment_repo);
     input_handler = new InputHandler(process_handler);
 }

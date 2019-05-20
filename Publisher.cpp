ff2b8c3f7d1c2ad485b44cdfc3e7a993f45b5684
ariangh001 <ariangh001@gmail.com>
2019-05-20 16:47:27 +0430

    implemented submitMovie method

--- Publisher.cpp
+++ Publisher.cpp
@@ -10,3 +10,12 @@ std::string Publisher::getType()
 {
     return "publisher";
 }
+
+void Publisher::submitMovie(Map input,MovieRepository* movie_repository)
+{
+    Movie* new_movie = new Movie(input["name"],stoi(input["year"]),
+        stoi(input["length"]),stoi(input["price"]),input["summary"],input["director"],0);
+    movie_repository->addMovie(new_movie);
+    published_films.push_back(new_movie);
+    //send_notif
+}

4d9f4c77691322aa85a2aa68290a1b2ab219a026
ariangh001 <ariangh001@gmail.com>
2019-05-20 16:48:07 +0430

    implemented editMovie method

--- Publisher.cpp
+++ Publisher.cpp
@@ -19,3 +19,29 @@ void Publisher::submitMovie(Map input,MovieRepository* movie_repository)
     published_films.push_back(new_movie);
     //send_notif
 }
+
+void Publisher::editMovieDetails(Map input,MovieRepository* movie_repository)
+{
+    if(input["film_id"] == "-1")
+    {
+        for(Counter i=0; i<published_films.size(); i++)
+        {
+            if(published_films[i]->getId() == stoi(input["film_id"]))
+            {
+                if(input["name"] != "-1")
+                    published_films[i]->setName(input["name"]);
+                else if(input["year"] == "-1")
+                    published_films[i]->setYear(stoi(input["year"]));
+                else if(input["length"] == "-1")
+                    published_films[i]->setLength(stoi(input["length"]));
+                else if(input["price"] == "-1")
+                    published_films[i]->setPrice(stoi(input["price"]));
+                else if(input["summary"] == "-1")
+                    published_films[i]->setSummary(input["summary"]);
+                else if(input["director"] == "-1")
+                    published_films[i]->setDirector(input["director"]);
+            }
+            break;
+        }
+    }
+}

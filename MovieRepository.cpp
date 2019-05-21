1aa0bf0a13b82f1268b76c3180ffced3e9627fbc
ariangh001 <ariangh001@gmail.com>
2019-05-21 23:24:42 +0430

    added claculateShare and wallet field

--- MovieRepository.cpp
+++ MovieRepository.cpp
@@ -3,6 +3,7 @@
 MovieRepository::MovieRepository()
 {
     last_id = 0;
+    wallet = 0;
 }
 
 void MovieRepository::addMovie(Movie* movie)
@@ -34,4 +35,28 @@ Movie* MovieRepository::findMovie(int film_id)
         if(movies[i]->getId() == film_id)
             return movies[i];
     throw NotFound();
+}
+
+void MovieRepository::increaseMoney(int amount)
+{
+    wallet += amount;
+}
+
+int MovieRepository::calculateShare(int film_id)
+{
+    int amount_returned = 0;
+    for(Counter i=0; i<movies.size(); i++)
+    {
+        if(movies[i]->getId() == film_id)
+        {
+            if(movies[i]->getRate() < 5)
+                amount_returned = 8 / 10 * movies[i]->getPrice();
+            else if(movies[i]->getRate() >= 5 && movies[i]->getRate() < 8)
+                amount_returned = 9 / 10 * movies[i]->getPrice();
+            else if(movies[i]->getPrice() >= 8)
+                amount_returned = 95 / 100 * movies[i]->getPrice();
+            wallet -= amount_returned;
+            return amount_returned;
+        }
+    }
 }
--- MovieRepository.h
+++ MovieRepository.h
@@ -12,12 +12,12 @@ class MovieRepository
         void eraseMovie(int movie_id);
         std::vector<Movie*> copyMovies(std::vector<Movie*> new_database);
         Movie* findMovie(int film_id);
-        //check kardane user id e jadid
-        //new kardan too khode add anjam beshe bekhatere check kardane id ba ozve akhare vector
-        //va new kardan ba tavajoh be oon anjam mishe
+        void increaseMoney(int money);
+        int calculateShare(int film_id);
     private:
         std::vector<Movie*> movies;
         int last_id;
+        int wallet;
 };
 
 #endif

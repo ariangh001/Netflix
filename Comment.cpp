f95d2f343f8304c15e401be4a5e200db617e0711
ariangh001 <ariangh001@gmail.com>
2019-05-23 15:34:12 +0430

    edited constructor

--- Comment.cpp
+++ Comment.cpp
@@ -4,8 +4,13 @@ Comment::Comment(std::string _content, int _user_id, int _movie_id)
 {
     id = 0;
     content = _content;
-    user_id = _user_id;
-    movie_id = _movie_id;
+    if(_user_id > 0 && _movie_id > 0)
+    {
+        user_id = _user_id;
+        movie_id = _movie_id;
+    }
+    else
+        throw BadRequest();
 }
 
 void Comment::setId(int _id)

2f29c098187eca8f5530378392275dc0afa429d2
ariangh001 <ariangh001@gmail.com>
2019-06-06 23:44:20 +0430

    implemented Login Handler callback method

--- Myfiles/RequestHandlers.cpp
+++ Myfiles/RequestHandlers.cpp
@@ -34,3 +34,45 @@ MovieDetails::MovieDetails(ProcessHandler* _handler)
 {
     handler = _handler;
 }
+
+Response* LoginHandler::callback(Request* req)
+{
+    try
+    {
+        Map loginInput = {{"username",""},{"password",""}};
+        loginInput["username"] = req->getBodyParam("username");
+        loginInput["password"] = req->getBodyParam("password");
+        handler->checkInput(loginInput,"login");
+        std::string session_id = handler->login(loginInput,"login");
+        Response* res = Response::redirect("/home");
+        res->setHeader("Content-Type","text/html");
+        res->setHeader("Developer-Name","ariyan");
+        res->setSessionId(session_id);
+        return res;
+    }
+    catch(std::exception& e)
+    {
+        std::string exc = e.what();
+        if(exc == "Bad Request")
+        {
+            Response* failed = Response::redirect("/badrequest");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+        if(exc == "Permission Denied")
+        {
+            Response* failed = Response::redirect("/permissionerror");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+        if(exc == "Not Found")
+        {
+            Response* failed = Response::redirect("/notfound");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+    }
+}

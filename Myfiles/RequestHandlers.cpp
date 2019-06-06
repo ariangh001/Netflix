b486e15556ade8a758089c6bbb7abb0cea9b48d5
ariangh001 <ariangh001@gmail.com>
2019-06-06 23:55:32 +0430

    implemented logout and signup callback method

--- Myfiles/RequestHandlers.cpp
+++ Myfiles/RequestHandlers.cpp
@@ -76,3 +76,88 @@ Response* LoginHandler::callback(Request* req)
         }
     }
 }
+
+Response* SignUpHandler::callback(Request* req)
+{
+    try
+    {
+        Map signUpInput = {{"email",""},{"username",""},
+            {"password",""},{"age",""},{"is_publisher",""}};
+        signUpInput["username"] = req->getBodyParam("username");
+        signUpInput["password"] = req->getBodyParam("password");
+        signUpInput["email"] = req->getBodyParam("email");
+        signUpInput["age"] = req->getBodyParam("age");
+        signUpInput["is_publisher"] = req->getBodyParam("is_publisher");
+        handler->checkInput(signUpInput,"signup");
+        std::string session_id = handler->signup(signUpInput,"signup");
+        Response* res = Response::redirect("/home");
+        res->setHeader("Content-Type","text/html");
+        res->setHeader("Developer-Name","ariyan");
+        res->setSessionId(session_id);
+        return res;
+    }
+    catch(std::exception& e)
+    {
+        std::string exc = e.what();
+        if(exc == "Bad Request")
+        {
+            Response* failed = Response::redirect("/badrequest");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+        if(exc == "Permission Denied")
+        {
+            Response* failed = Response::redirect("/permissionerror");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+        if(exc == "Not Found")
+        {
+            Response* failed = Response::redirect("/notfound");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+    }
+}
+
+Response* LogoutHandler::callback(Request* req)
+{
+    try
+    {
+        std::string req_session = req->getSessionId();
+        handler->logout(req_session);
+        Response* res = Response::redirect("/");
+        res->setHeader("Content-Type","text/html");
+        res->setHeader("Developer-Name","ariyan");
+        res->setSessionId("");
+        return res;
+    }
+    catch(std::exception& e)
+    {
+                std::string exc = e.what();
+        if(exc == "Bad Request")
+        {
+            Response* failed = Response::redirect("/badrequest");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+        if(exc == "Permission Denied")
+        {
+            Response* failed = Response::redirect("/permissionerror");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+        if(exc == "Not Found")
+        {
+            Response* failed = Response::redirect("/notfound");
+            failed->setHeader("Content-Type","text/html");
+            failed->setHeader("Developer-Name","ariyan");
+            return failed;
+        }
+    }
+}
